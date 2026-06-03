2772b754c4b77f66aaf45069d9c432517a56953e
Alana Fiorelini <alanaetec3@gmail.com>
2026-06-03 17:55:02 -0300

    Add files via upload

--- produtos.sql
+++ produtos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Maio-2026 às 22:50
+-- Tempo de geração: 03-Jun-2026 às 22:54
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 7.4.29
 
